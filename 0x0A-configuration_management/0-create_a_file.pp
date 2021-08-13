13cc43c52ac4e3e997e255c7f019a59051a00d47
livejo <yoseph.kefale@gmail.com>
2021-08-13 16:44:36 +0300

    Create 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#create a file holberton
+file {'/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
